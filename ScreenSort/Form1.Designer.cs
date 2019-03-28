5a56cd579ec025795019ecc571315415ce9998f3
Michał Kisiel <mkisiel58@gmail.com>
2019-03-28 21:39:50 +0100

    Basic screenshot

--- ScreenSort/Form1.Designer.cs
+++ ScreenSort/Form1.Designer.cs
@@ -28,13 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.buttonScreenshot = new System.Windows.Forms.Button();
+            this.buttonShowScreen = new System.Windows.Forms.Button();
+            this.pictureBoxPreview = new System.Windows.Forms.PictureBox();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPreview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.buttonScreenshot);
+            this.flowLayoutPanel1.Controls.Add(this.buttonShowScreen);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(624, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(200, 463);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // buttonScreenshot
+            // 
+            this.buttonScreenshot.Location = new System.Drawing.Point(3, 3);
+            this.buttonScreenshot.Name = "buttonScreenshot";
+            this.buttonScreenshot.Size = new System.Drawing.Size(75, 23);
+            this.buttonScreenshot.TabIndex = 0;
+            this.buttonScreenshot.Text = "Screenshot";
+            this.buttonScreenshot.UseVisualStyleBackColor = true;
+            this.buttonScreenshot.Click += new System.EventHandler(this.buttonScreenshot_Click);
+            // 
+            // buttonShowScreen
+            // 
+            this.buttonShowScreen.Location = new System.Drawing.Point(84, 3);
+            this.buttonShowScreen.Name = "buttonShowScreen";
+            this.buttonShowScreen.Size = new System.Drawing.Size(75, 23);
+            this.buttonShowScreen.TabIndex = 1;
+            this.buttonShowScreen.Text = "Preview";
+            this.buttonShowScreen.UseVisualStyleBackColor = true;
+            this.buttonShowScreen.Click += new System.EventHandler(this.buttonShowScreen_Click);
+            // 
+            // pictureBoxPreview
+            // 
+            this.pictureBoxPreview.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxPreview.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxPreview.Name = "pictureBoxPreview";
+            this.pictureBoxPreview.Size = new System.Drawing.Size(624, 463);
+            this.pictureBoxPreview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxPreview.TabIndex = 1;
+            this.pictureBoxPreview.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(824, 463);
+            this.Controls.Add(this.pictureBoxPreview);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPreview)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button buttonScreenshot;
+        private System.Windows.Forms.Button buttonShowScreen;
+        private System.Windows.Forms.PictureBox pictureBoxPreview;
     }
 }
 
--- ScreenSort/Form1.cs
+++ ScreenSort/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,23 @@ namespace ScreenSort
 {
     public partial class Form1 : Form
     {
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void buttonScreenshot_Click(object sender, EventArgs e)
+        {
+            ImageManipulator im = new ImageManipulator();
+            im.CreateScreenshot(); 
+        }
+
+        private void buttonShowScreen_Click(object sender, EventArgs e)
+        {
+            ImageManipulator im = new ImageManipulator();
+            im.ShowPreview(pictureBoxPreview);
         }
     }
 }
